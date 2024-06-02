1df0a127cb14361eaba956db98ffa5618708fa51
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2024-06-02 09:18:38 +0300

    customer service added RestTemplate

--- src/main/java/sentinelguardcustomer_service/entity/Customer.java
+++ src/main/java/sentinelguardcustomer_service/entity/Customer.java
@@ -3,12 +3,14 @@ package sentinelguardcustomer_service.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Data
 @Table(name = "customer")
 public class Customer {
 
--- src/main/java/sentinelguardcustomer_service/service/CustomerService.java
+++ src/main/java/sentinelguardcustomer_service/service/CustomerService.java
@@ -1,20 +1,37 @@
 package sentinelguardcustomer_service.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import sentinelguardcustomer_service.dto.CustomerRegistrationRequest;
+import sentinelguardcustomer_service.dto.FraudCheckResponse;
 import sentinelguardcustomer_service.entity.Customer;
 import sentinelguardcustomer_service.repository.CustomerRepository;
 
 @Service
 @Slf4j
-public record CustomerService(CustomerRepository customerRepository) {
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
-        customerRepository.save(customer);
+
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8086/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
     }
 }
